81f95e0cdc8421e95d17d558ae8f935d0a107ff9
Laura Rocha Prado <lauraprado@asu.edu>
2020-06-05 15:58:06 -0700

    Updates

---
+++ precache-manifest.adf6f896150510f9964e3cd4bdc047bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8bf7112b6f40512b2aac5834cdf2eb",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "15e5e2d704d81ef31c9f",
+    "url": "/recipes/static/css/main.fca048b0.chunk.css"
+  },
+  {
+    "revision": "3ecb34a03ccd6313df20",
+    "url": "/recipes/static/js/2.bd7bdbdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.bd7bdbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e5e2d704d81ef31c9f",
+    "url": "/recipes/static/js/main.444ccc32.chunk.js"
+  },
+  {
+    "revision": "c8ab3bf18217eb1e5c85",
+    "url": "/recipes/static/js/runtime-main.3b55c3fa.js"
+  }
+]);
